0b81835fc0a3ed8309d3e43c4af714ae32e35bfa
josephajibodu <josephajibodu@gmail.com>
2025-06-17 20:48:52 +0100

    topup fix

--- app/Filament/Resources/TopUpResource/Pages/ListTopUps.php
+++ app/Filament/Resources/TopUpResource/Pages/ListTopUps.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\TopUpResource\Pages;
 
+use App\Enums\TopupStatus;
 use App\Filament\Resources\TopUpResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTopUps extends ListRecords
 {
@@ -15,5 +18,21 @@ class ListTopUps extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'pending' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', TopupStatus::Pending)),
+            'completed' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', TopupStatus::Completed)),
+        ];
+    }
+
+    public function getDefaultActiveTab(): string | int | null
+    {
+        return 'pending';
     }
 }
