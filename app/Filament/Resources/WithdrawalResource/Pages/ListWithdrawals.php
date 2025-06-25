97c01b7499bcf03b5346fff8e0b91f191f039e93
josephajibodu <josephajibodu@gmail.com>
2025-06-25 17:43:02 +0100

    updates

--- app/Filament/Resources/WithdrawalResource/Pages/ListWithdrawals.php
+++ app/Filament/Resources/WithdrawalResource/Pages/ListWithdrawals.php
@@ -13,7 +13,7 @@ class ListWithdrawals extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
