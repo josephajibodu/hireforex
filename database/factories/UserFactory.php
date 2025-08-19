7b107bbaeb31379bf3278f456420630209e508de
josephajibodu <josephajibodu@gmail.com>
2025-08-19 23:53:03 +0100

    admin fixed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,18 +34,12 @@ class UserFactory extends Factory
             'last_name' => $lastname = fake()->lastName(),
             'username' => $username = fake()->userName(),
             'phone_number' => fake()->phoneNumber,
-            'whatsapp_number' => fake()->phoneNumber,
 
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
 
-            'bank_name' => fake()->randomElement(['Access Bank', 'Opay', 'Kuda', 'GT Co', 'Moniepoint']),
-            'bank_account_name' => "$firstname $lastname",
-            'bank_account_number' => Str::padLeft(fake()->randomNumber(8), 10),
-
-            'referral_code' => Str::random(10)
         ];
     }
 
@@ -62,13 +56,6 @@ class UserFactory extends Factory
     public function configure(): UserFactory
     {
         return $this->afterCreating(function (User $user) {
-
-            KycVerification::query()->create([
-                'user_id' => $user->id,
-                'document' => '',
-                'document_type' => KYCDocumentType::DriversLicense,
-                'status' => KYCStatus::Completed
-            ]);
 
             // $user->credit(10, 'Factory bonus from Profitchain');
         });
